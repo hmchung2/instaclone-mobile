6faf4a39b5c0b08086598f5075592c164f05eaa5
hmchung2 <hmchung2@gmail.com>
2023-02-12 20:20:53 +0900

    15.8

--- components/auth/AuthLayout.js
+++ components/auth/AuthLayout.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: #1e272e;
+  background-color: black;
   padding: 0px 40px;
 `;
 
@@ -17,10 +18,15 @@ const Logo = styled.Image`
 `;
 
 export default function AuthLayout({ children }) {
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
   return (
-    <Container>
-      <Logo resizeMode="contain" source={require("../../assets/logo.png")} />
-      {children}
-    </Container>
+    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={dismissKeyboard}>
+      <Container>
+        <Logo resizeMode="contain" source={require("../../assets/logo.png")} />
+        {children}
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
