2a905662adf3288b6c92cc2d25732b726bd4c5b7
hmchung2 <hmchung2@gmail.com>
2023-02-12 21:00:20 +0900

    commit

--- components/auth/AuthLayout.js
+++ components/auth/AuthLayout.js
@@ -19,15 +19,20 @@ const Logo = styled.Image`
   max-width: 50%;
   width: 100%;
   height: 100px;
+  margin: 0 auto;
   margin-bottom: 20px;
 `;
 
 export default function AuthLayout({ children }) {
   const dismissKeyboard = () => {
-    Keyboard.dismiss();
+    // Keyboard.dismiss();
   };
   return (
-    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={dismissKeyboard}>
+    <TouchableWithoutFeedback
+      style={{ flex: 1 }}
+      onPress={dismissKeyboard}
+      disabled={Platform.OS === "web"}
+    >
       <Container>
         <KeyboardAvoidingView
           style={{
