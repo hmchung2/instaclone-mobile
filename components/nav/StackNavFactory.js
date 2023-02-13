86315ebabd83fbb0f93ac03082559da1529160a8
hmchung2 <hmchung2@gmail.com>
2023-02-14 00:32:09 +0900

    16.3

--- components/nav/StackNavFactory.js
+++ components/nav/StackNavFactory.js
@@ -11,7 +11,17 @@ const Stack = createStackNavigator();
 
 export default function StackNavFactory({ screenName }) {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: true,
+        headerTitleAlign: "center",
+        headerTintColor: "white",
+        headerStyle: {
+          shadowColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+        },
+      }}
+    >
       {screenName === "Feed" ? (
         <Stack.Screen name={"Feed"} component={Feed} />
       ) : null}
@@ -22,8 +32,8 @@ export default function StackNavFactory({ screenName }) {
         <Stack.Screen name={"Notifications"} component={Notifications} />
       ) : null}
       {screenName === "Me" ? <Stack.Screen name={"Me"} component={Me} /> : null}
-      <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Photo" component={Photo} />
+      <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
   );
 }
