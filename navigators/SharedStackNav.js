1152e6908a322009a3459da9ce6f160ded486d3e
hmchung2 <hmchung2@gmail.com>
2023-02-14 22:39:04 +0900

    16.8

--- navigators/SharedStackNav.js
+++ navigators/SharedStackNav.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import Photo from "../screens/Photo";
 import Profile from "../screens/Profile";
 import Feed from "../screens/Feed";
 import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Me from "../screens/Me";
 import { Image } from "react-native";
-import Photo from "../components/Photo";
+import Likes from "../screens/Likes";
+import Comments from "../screens/Comments";
 
 const Stack = createStackNavigator();
 
@@ -14,8 +16,8 @@ export default function SharedStackNav({ screenName }) {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerMode: "screen",
         headerBackTitleVisible: false,
+        headerMode: "screen",
         headerTintColor: "white",
         headerTitleAlign: "center",
         headerStyle: {
@@ -52,6 +54,8 @@ export default function SharedStackNav({ screenName }) {
       {screenName === "Me" ? <Stack.Screen name={"Me"} component={Me} /> : null}
       <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Photo" component={Photo} />
+      <Stack.Screen name="Likes" component={Likes} />
+      <Stack.Screen name="Comments" component={Comments} />
     </Stack.Navigator>
   );
 }
