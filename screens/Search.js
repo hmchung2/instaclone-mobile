8b6fc33839b75354a0a25a72abb7b6ecbcd5dcc6
hmchung2 <hmchung2@gmail.com>
2023-02-15 23:54:40 +0900

    17.4

--- screens/Search.js
+++ screens/Search.js
@@ -1,14 +1,24 @@
+import { gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Text, TextInput, View } from "react-native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../components/DismissKeyboard";
 
+const SEARCH_PHOTOS = gql`
+  query searchPhotos($keyword: String!) {
+    searchPhotos(keyword: $keyword) {
+      id
+      file
+    }
+  }
+`;
+
 const Input = styled.TextInput``;
 
 export default function Search({ navigation }) {
   const { setValue, register } = useForm();
-
+  const [startQueryFn, { loading, data }] = useLazyQuery(SEARCH_PHOTOS);
   const SearchBox = () => (
     <TextInput
       style={{ backgroundColor: "white" }}
@@ -21,14 +31,12 @@ export default function Search({ navigation }) {
       onChangeText={(text) => setValue("keyword", text)}
     />
   );
-
   useEffect(() => {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
     register("keyword");
   }, []);
-
   return (
     <DismissKeyboard>
       <View
